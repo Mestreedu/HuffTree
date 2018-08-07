2809d56edd888b202d3f3527892c2f4dc31a4fab
felipeescorel <felipe.escorel@gmail.com>
2018-08-07 01:41:29 -0300

    Commit

--- HuffTree.hs
+++ HuffTree.hs
@@ -7,45 +7,59 @@ import Data.Function
 
 
 --Monta a tabela de uma determinada string
-montaTabela:: String->[(Char,Int)]
-montaTabela a = zip (tiraRep a) (contaRep a)
+montaTabela:: String->[([Char],Int)]
+montaTabela a = zip (map (goList)(tiraRep a)) (contaRep a)
 
+--Converte um Char pra uma String. Para manipulação.
+goList::Char->[Char]
+goList a = [a]
  --Tira Repetição
 tiraRep:: String->[Char]
 tiraRep [] = []    
-tiraRep(x:xs) = x:(tiraRep(filter(/=x) xs)) 
+tiraRep(x:xs) = x:(tiraRep(filter(/=x) xs))
  --Conta Repetição
 contaRep:: String->[Int]
 contaRep [] = []
 contaRep (x:xs) = 1+(length (filter(== x) xs)):(contaRep[y|y<-xs,y/=x])
 
 --Ordena em ordem crescente (QuickSort)
-ordena::[(Char,Int)]->[(Char,Int)]
+ordena::[([Char],Int)]->[([Char],Int)]
 ordena [] = []
 ordena (x:xs) = ordena[y|y<- xs,(snd y)<(snd x)] ++ [x]++ ordena[y|y<- xs,snd y>=(snd x)]
 
 --Estrutura de árvore
-data Arvore = No ([Char],Int) Arvore Arvore|Nulo
+data Arvore a = No a (Arvore a) (Arvore a)|Folha a
 
---Auxiliar
-aux a =((fst a):[],snd a) 
+--Soma duas folhas
+somaFilhos:: Arvore ([Char],Int)-> Arvore ([Char],Int)->Arvore ([Char],Int)
+somaFilhos a b = No (aglutina (transform a) (transform b)) (a) (b)
 
 --Transforma uma tupla numa folha
-tuplaFolha:: (Char,Int)->Arvore
-tuplaFolha a = No (aux a) (Nulo) (Nulo)
+tuplaFolha:: (String,Int)->Arvore (String,Int)
+tuplaFolha a = Folha a
 
 --Monta uma lista com os elementos que serão as folhas da árvore
-montaFolhas::String->[Arvore]
+montaFolhas::String->[Arvore (String,Int)]
 montaFolhas [] = []
 montaFolhas x = (tuplaFolha (head (ordena(montaTabela x)))):(montaFolhas  (tail x))
 
---Muda o tipo do identificador Char pra uma String e soma os inteiros
---Usar pros valores dos nós
-aglutina::(Char,Int)->(Char,Int)->([Char],Int)
-aglutina a b = ((fst a):[fst b], (snd a)+(snd b))
+--Soma duas tuplas.Usar pros valores dos nós
+aglutina::([Char],Int)->([Char],Int)->([Char],Int)
+aglutina a b = (fst a ++ fst b, (snd a)+(snd b))
 
+--Serializa o valor da árvore. Para manipular as tuplas
+transform:: Arvore (String,Int)-> (String,Int)
+transform (Folha a) = a
+transform (No a f1 f2) = a  
 
-----Decodificando uma string binária
+--montaPai::[Arvore ([Char],Int)]->[Arvore ([Char],Int)]->Arvore ([Char],Int)
+--montaPai a b = somaFilhos a b
+
+--Monta a Arvore
+--montaArvore::[Arvore ([Char],Int)]->[Arvore([Char],Int)]
+--montaArvore [] = []
+--montaArvore (x:xs:xxs) = No (montaPai x xs)
+---Decodificando uma string binária
 --decodifica::Arvore->String->String
 --decodifica raiz string = aux raiz string where
 --aux(Folha c) string = c:(aux raiz string)
